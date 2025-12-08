b7309999636eb9465aa883a696b4813177e9e625
Anatoliy Dimitrov <anatoliy.dimitrov@sap.com>
2025-12-08 05:45:18 +0200

    Runnn

--- src/albums/cats_thumbs/thumbnail_creator.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in `ls`
-do
-convert "$i" -resize 140x140 "TN_${i}"
-echo $i processed
-done
--- src/albums/thumbnail_creator.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find -name '*_TN' -exec rm -rf
-for i in `find -name '*.jpg'`
-do
-convert "$i" -resize 140x140 "${i}_TN"
-echo $i processed
-done
